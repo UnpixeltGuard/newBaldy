2c4b0ea7e25fb23d49a9fb7978f80dd96d1db41e
UnpixeltGuard <118729006+UnpixeltGuard@users.noreply.github.com>
2024-11-21 23:21:43 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ if [ ! -f "$FILE" ]; then
     echo "No config.txt found, downloading example config from
     https://github.com/UnpixeltGuard/newBaldy"
     
-    curl -L https://raw.githubusercontent.com/UnpixeltGuard/newBaldy/refs/heads/main/config.txt?token=GHSAT0AAAAAACYEW3M6QHLYDASRVK2NVFKKZZ7TLAQ -o /app/config.txt
+    curl -L https://raw.githubusercontent.com/UnpixeltGuard/newBaldy/refs/heads/main/config.txt -o /app/config.txt
     echo ""
 fi
 
